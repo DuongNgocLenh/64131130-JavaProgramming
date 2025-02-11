8d2684a701a1ecf2301cee9d058b7bdf402ea400
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-02-11 19:33:08 +0700

    Ví dụ đầu tiên
    Demo

---
+++ HelloWorld/src/till/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package till.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		System.out.println("Tôi lần đầu học Java, và đang thử nghiệm quản lý mã nguồn với GIT");
+
+	}
+
+}
